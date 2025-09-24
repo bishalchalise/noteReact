04fdc3f6e529dbd84190139578a3f5cd91eed3f3
Bishal Chalise <bishalise@gmail.com>
2025-09-24 13:22:12 +0200

    integrate

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -1,70 +1,49 @@
+require("dotenv").config();
 const express = require("express");
+const Note = require("./models/note");
+
 const app = express();
 app.use(express.json());
 app.use(express.static("dist"));
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+let notes = [];
 app.get("/", (request, response) => {
   response.send("<h1>hello world</h1>");
 });
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
+
 app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
+// app.delete("/api/notes/:id", (request, response) => {
+//   const id = request.params.id;
+//   notes = notes.filter((note) => note.id !== id);
 
-  response.status(204).end();
-});
+//   response.status(204).end();
+// });
 
-const generateId = () => {
-  const MaxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(MaxId + 1);
-};
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
   if (!body.content) {
-    response.status(400).json({
-      error: "content-missing",
-    });
+    return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 const PORT = process.env.PORT || 3001;
