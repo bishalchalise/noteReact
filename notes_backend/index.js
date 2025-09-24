ed0764ce6aceea75af3bdc7d6e363bfde0b0fd84
Bishal Chalise <bishalise@gmail.com>
2025-09-24 17:38:12 +0200

    uibuild

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -3,8 +3,18 @@ const express = require("express");
 const Note = require("./models/note");
 
 const app = express();
-app.use(express.json());
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
 app.use(express.static("dist"));
+app.use(express.json());
 
 let notes = [];
 app.get("/", (request, response) => {
@@ -16,18 +26,25 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-// app.delete("/api/notes/:id", (request, response) => {
-//   const id = request.params.id;
-//   notes = notes.filter((note) => note.id !== id);
-
-//   response.status(204).end();
-// });
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
@@ -45,6 +62,31 @@ app.post("/api/notes", (request, response) => {
     response.json(savedNote);
   });
 });
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
+
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (!note) {
+        return response.status(404).end();
+      }
+
+      note.content = content;
+      note.important = important;
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote);
+      });
+    })
+    .catch((error) => next(error));
+});
+const unknownEndpoint = (request, response) => {
+  return response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
