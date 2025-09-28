515c127aec481ee814374abb4918f2e9f2e295b9
Bishal Chalise <bishalise@gmail.com>
2025-09-28 23:01:38 +0200

    middleware

---
+++ notes_backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const noteRouter = require('./controller/notes')
+const middleware = require('./utils/middleware')
+
+const app = express()
+
+logger.info('connecting to ', config.MONGODB_URI)
+
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to mongoDB')
+    })
+    .catch(error => {
+        logger.error('error connecting to mongoDB', error.message)
+    })
+
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/notes', noteRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
